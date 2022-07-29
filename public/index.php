faa75310345f9ba9a803cfa20996e863463dddea
Manish Jakhode <manish@Manishs-MacBook-Air.local>
2022-07-30 00:46:04 +0530

    #1- renderView issue for excepting array in routes

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ $app = new Application(dirname(__DIR__));
 
 
 $app->router->get('/', 'home');
+// @todo - handle array property for rendering views from controller.
 $app->router->get('/contacts', [SiteController::class, 'contact']);
 $app->router->post('/contacts', [ SiteController::class, 'handle']);
 
