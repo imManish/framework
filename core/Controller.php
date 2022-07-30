7495bd6f35ae72ee5210e7db264865a6b3168d4d
Manish J <manish.j@knights.com>
2022-07-30 20:33:23 +0530

    #4 - Auth Dynamic Layout integration.

--- core/Controller.php
+++ core/Controller.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Core;
+use BadMethodCallException;
 
-class Controller
+abstract class Controller
 {
+    public string $layout = '_base';
     /**
+     * Routing render view method.
+     *
      * @param $view
      * @param $param
      * @return array|false|string|string[]
@@ -14,4 +18,23 @@ class Controller
         return Application::$app->router->renderView($view, $param);
     }
 
+
+    /**
+     * Routing Controller
+     *
+     * @param $method
+     * @param $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        throw new BadMethodCallException(sprintf(
+            'Method %s::%s does not exist.', static::class, $method
+        ));
+    }
+
+    public function layout(string $layout)
+    {
+        $this->layout = $layout;
+    }
 }
